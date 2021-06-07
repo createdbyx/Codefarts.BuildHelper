13356453e8458cb6cde6fb59ed479c929ba3c542
Dean <deanlunz@createdbyx.com>
2021-06-07 03:57:07 -0700

    added more unit tests

--- BuildHelperTests/ConditionNotEquality.cs
+++ BuildHelperTests/ConditionNotEquality.cs
@@ -1,4 +1,4 @@
-// <copyright file="ConditionEquality.cs" company="Codefarts">
+﻿// <copyright file="ConditionNotEquality.cs" company="Codefarts">
 // Copyright (c) Codefarts
 // contact@codefarts.com
 // http://www.codefarts.com
@@ -13,36 +13,26 @@ namespace BuildHelperTests
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     [TestClass, TestCategory("Extension Methods")]
-    public class ConditionEquality
+    public class ConditionNotEquality
     {
-        private XElement goodConditionEquality;
-        private XElement goodConditionEqualityWithTrueIgnore;
-        private XElement goodConditionEqualityWithFalseIgnore;
         private IDictionary<string, object> varibles;
 
         [TestInitialize]
         public void InitTest()
         {
-            this.goodConditionEquality = new XElement("condition", new XAttribute("value1", "Test"), new XAttribute("operator", "="),
-                new XAttribute("value2", "Test"));
-            this.goodConditionEqualityWithTrueIgnore = new XElement("condition", new XAttribute("value1", "Test"), new XAttribute("operator", "="),
-                new XAttribute("value2", "Test"), new XAttribute("ignorecase", true));
-            this.goodConditionEqualityWithFalseIgnore = new XElement("condition", new XAttribute("value1", "Test"), new XAttribute("operator", "="),
-                new XAttribute("value2", "Test"), new XAttribute("ignorecase", false));
             this.varibles = new Dictionary<string, object>();
         }
 
         [TestCleanup]
         public void TestCleanup()
         {
-            this.goodConditionEquality = null;
             this.varibles = null;
         }
 
         [TestMethod]
         public void GoodButValues2NotPresent_MissingIgnore()
         {
-            var item = XElement.Parse("<condition value1=\"Test\" operator=\"=\" />");
+            var item = XElement.Parse("<condition value1=\"Test\" operator=\"!=\" />");
             var node = TestHelpers.BuildCommandNode(item, null);
             Assert.ThrowsException<ArgumentOutOfRangeException>(() => node.SatifiesCondition(this.varibles));
         }
@@ -50,7 +40,7 @@ namespace BuildHelperTests
         [TestMethod]
         public void GoodButValues1NotPresent_MissingIgnore()
         {
-            var item = XElement.Parse("<condition operator=\"=\" value2=\"Test\" />");
+            var item = XElement.Parse("<condition operator=\"!=\" value2=\"Test\" />");
             var node = TestHelpers.BuildCommandNode(item, null);
             Assert.ThrowsException<ArgumentOutOfRangeException>(() => node.SatifiesCondition(this.varibles));
         }
@@ -58,95 +48,95 @@ namespace BuildHelperTests
         [TestMethod]
         public void GoodButMissingValues2_MissingIgnore()
         {
-            var item = XElement.Parse("<condition value1=\"Test\" operator=\"=\" value2=\"\" />");
+            var item = XElement.Parse("<condition value1=\"Test\" operator=\"!=\" value2=\"\" />");
             var node = TestHelpers.BuildCommandNode(item, null);
-            Assert.IsFalse(node.SatifiesCondition(this.varibles));
+            Assert.IsTrue(node.SatifiesCondition(this.varibles));
         }
 
         [TestMethod]
         public void GoodButMissingValues1_MissingIgnore()
         {
-            var item = XElement.Parse("<condition value1=\"\" operator=\"=\" value2=\"Test\" />");
+            var item = XElement.Parse("<condition value1=\"\" operator=\"!=\" value2=\"Test\" />");
             var node = TestHelpers.BuildCommandNode(item, null);
-            Assert.IsFalse(node.SatifiesCondition(this.varibles));
+            Assert.IsTrue(node.SatifiesCondition(this.varibles));
         }
 
         [TestMethod]
         public void GoodButValuesDiffer_MissingIgnore()
         {
-            var item = XElement.Parse("<condition value1=\"Zest\" operator=\"=\" value2=\"Test\" />");
+            var item = XElement.Parse("<condition value1=\"Zest\" operator=\"!=\" value2=\"Test\" />");
             var node = TestHelpers.BuildCommandNode(item, null);
-            Assert.IsFalse(node.SatifiesCondition(this.varibles));
+            Assert.IsTrue(node.SatifiesCondition(this.varibles));
         }
 
         [TestMethod]
-        public void GoodButValuesDiffer_Equal_MissingIgnore()
+        public void GoodButValuesDiffer_NotEqual_MissingIgnore()
         {
-            var item = XElement.Parse("<condition value1=\"Zest\" operator=\"equals\" value2=\"Test\" />");
+            var item = XElement.Parse("<condition value1=\"Zest\" operator=\"notequal\" value2=\"Test\" />");
             var node = TestHelpers.BuildCommandNode(item, null);
-            Assert.IsFalse(node.SatifiesCondition(this.varibles));
+            Assert.IsTrue(node.SatifiesCondition(this.varibles));
         }
 
         [TestMethod]
-        public void GoodButValuesDiffer_EqualTo_MissingIgnore()
+        public void GoodButValuesDiffer_NotEqualTo_MissingIgnore()
         {
-            var item = XElement.Parse("<condition value1=\"Zest\" operator=\"equalto\" value2=\"Test\" />");
+            var item = XElement.Parse("<condition value1=\"Zest\" operator=\"notequalto\" value2=\"Test\" />");
             var node = TestHelpers.BuildCommandNode(item, null);
-            Assert.IsFalse(node.SatifiesCondition(this.varibles));
+            Assert.IsTrue(node.SatifiesCondition(this.varibles));
         }
 
         [TestMethod]
         public void SameValuesSameCasing_MissingIgnore()
         {
-            var item = XElement.Parse("<condition value1=\"Test\" operator=\"=\" value2=\"Test\" />");
-            var node = TestHelpers.BuildCommandNode(item, null);
-            Assert.IsTrue(node.SatifiesCondition(this.varibles));
-        }
-
-        [TestMethod]
-        public void SameValuesDiffernentCasing_MissingIgnore()
-        {
-            var item = XElement.Parse("<condition value1=\"test\" operator=\"=\" value2=\"Test\" />");
-            var node = TestHelpers.BuildCommandNode(item, null);
-            Assert.IsTrue(node.SatifiesCondition(this.varibles));
-        }
-
-        [TestMethod]
-        public void SameValuesDiffernentCasing_IgnoreSetToFalse()
-        {
-            var item = XElement.Parse("<condition value1=\"test\" operator=\"=\" value2=\"Test\" ignorecase=\"false\" />");
+            var item = XElement.Parse("<condition value1=\"Test\" operator=\"!=\" value2=\"Test\" />");
             var node = TestHelpers.BuildCommandNode(item, null);
             Assert.IsFalse(node.SatifiesCondition(this.varibles));
         }
 
         [TestMethod]
-        public void SameValuesDiffernentCasing_IgnoreSetToTrue()
+        public void SameValuesDiffernentCasing_MissingIgnore()
         {
-            var item = XElement.Parse("<condition value1=\"test\" operator=\"=\" value2=\"Test\" ignorecase=\"true\" />");
+            var item = XElement.Parse("<condition value1=\"test\" operator=\"!=\" value2=\"Test\" />");
+            var node = TestHelpers.BuildCommandNode(item, null);
+            Assert.IsFalse(node.SatifiesCondition(this.varibles));
+        }
+
+        [TestMethod]
+        public void SameValuesDiffernentCasing_IgnoreSetToFalse()
+        {
+            var item = XElement.Parse("<condition value1=\"test\" operator=\"!=\" value2=\"Test\" ignorecase=\"false\" />");
             var node = TestHelpers.BuildCommandNode(item, null);
             Assert.IsTrue(node.SatifiesCondition(this.varibles));
+        }
+
+        [TestMethod]
+        public void SameValuesDiffernentCasing_IgnoreSetToTrue()
+        {
+            var item = XElement.Parse("<condition value1=\"test\" operator=\"!=\" value2=\"Test\" ignorecase=\"true\" />");
+            var node = TestHelpers.BuildCommandNode(item, null);
+            Assert.IsFalse(node.SatifiesCondition(this.varibles));
         }
 
         [TestMethod]
         public void SameValuesDiffernentCasing_IgnoreSetToTRUE()
         {
-            var item = XElement.Parse("<condition value1=\"test\" operator=\"=\" value2=\"Test\" ignorecase=\"TRUE\" />");
+            var item = XElement.Parse("<condition value1=\"test\" operator=\"!=\" value2=\"Test\" ignorecase=\"TRUE\" />");
             var node = TestHelpers.BuildCommandNode(item, null);
-            Assert.IsTrue(node.SatifiesCondition(this.varibles));
+            Assert.IsFalse(node.SatifiesCondition(this.varibles));
         }
 
         [TestMethod]
         public void GoodButMissingValues_MissingIgnore()
         {
-            var item = XElement.Parse("<condition value1=\"\" operator=\"=\" value2=\"\" />");
+            var item = XElement.Parse("<condition value1=\"\" operator=\"!=\" value2=\"\" />");
             var node = TestHelpers.BuildCommandNode(item, null);
-            Assert.IsTrue(node.SatifiesCondition(this.varibles));
+            Assert.IsFalse(node.SatifiesCondition(this.varibles));
         }
 
         [TestMethod]
         public void GoodButMissingValues_IgnoreNotSpecified()
         {
-            var item = XElement.Parse("<condition value1=\"\" operator=\"=\" value2=\"\" ignorecase=\"\" />");
+            var item = XElement.Parse("<condition value1=\"\" operator=\"!=\" value2=\"\" ignorecase=\"\" />");
             var node = TestHelpers.BuildCommandNode(item, null);
             Assert.ThrowsException<ArgumentOutOfRangeException>(() => node.SatifiesCondition(this.varibles));
         }
@@ -170,7 +160,7 @@ namespace BuildHelperTests
         [TestMethod]
         public void GoodButMissingValues_BadIgnoreValueSpecified()
         {
-            var item = XElement.Parse("<condition value1=\"\" operator=\"=\" value2=\"\" ignorecase=\"Bad\" />");
+            var item = XElement.Parse("<condition value1=\"\" operator=\"!=\" value2=\"\" ignorecase=\"Bad\" />");
             var node = TestHelpers.BuildCommandNode(item, null);
             Assert.ThrowsException<ArgumentOutOfRangeException>(() => node.SatifiesCondition(this.varibles));
         }
@@ -184,9 +174,9 @@ namespace BuildHelperTests
         [TestMethod]
         public void VariblesArgNull()
         {
-            var item = XElement.Parse("<condition value1=\"\" operator=\"=\" value2=\"\" />");
+            var item = XElement.Parse("<condition value1=\"\" operator=\"!=\" value2=\"\" />");
             var node = TestHelpers.BuildCommandNode(item, null);
-            Assert.IsTrue(node.SatifiesCondition(null));
+            Assert.IsFalse(node.SatifiesCondition(null));
         }
 
         [TestMethod]
@@ -198,7 +188,7 @@ namespace BuildHelperTests
         [TestMethod]
         public void ElementNameNotCondition()
         {
-            var item = XElement.Parse("<badcondition value1=\"\" operator=\"=\" value2=\"\" />");
+            var item = XElement.Parse("<badcondition value1=\"\" operator=\"!=\" value2=\"\" />");
             var node = TestHelpers.BuildCommandNode(item, null);
             Assert.ThrowsException<ArgumentException>(() => node.SatifiesCondition(this.varibles));
         }
--- BuildHelperTests/ExtensionMethodTests.cs
+++ BuildHelperTests/ExtensionMethodTests.cs
@@ -6,6 +6,7 @@
 
 namespace BuildHelperTests
 {
+    using System;
     using System.Collections.Generic;
     using System.IO;
     using Codefarts.BuildHelper;
@@ -87,6 +88,211 @@ namespace BuildHelperTests
 
             var expected = "$(ProjectDir)";
             Assert.AreEqual(expected, text);
+        }
+
+        [TestMethod]
+        public void GetValueWithNullParameters()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => { ExtensionMethods.GetValue<string>(null, "test", string.Empty); });
+        }
+
+        [TestMethod]
+        public void RunCommandArgsGetParameterWithNullArgsAndDefaultValue()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() =>
+            {
+                RunCommandArgs args = null;
+                ExtensionMethods.GetParameter<string>(args, "test", string.Empty);
+            });
+        }
+
+        [TestMethod]
+        public void RunCommandArgsGetParameterWithMissingParameterAndDefaultValue()
+        {
+            var data = new CommandData();
+            var args = new RunCommandArgs(null, new Dictionary<string, object>(), data, new BuildHelper());
+            var value = ExtensionMethods.GetParameter<string>(args, "test", "default");
+            Assert.AreEqual("default", value);
+        }
+
+        [TestMethod]
+        public void RunCommandArgsGetParameterWithValidParameterAndNoDefaultValue()
+        {
+            var data = new CommandData();
+            data.Parameters["test"] = "value";
+            var args = new RunCommandArgs(null, new Dictionary<string, object>(), data, new BuildHelper());
+            var value = ExtensionMethods.GetParameter<string>(args, "test");
+            Assert.AreEqual("value", value);
+        }
+
+        [TestMethod]
+        public void RunCommandArgsGetParameterWithValidParameterAndNoDefaultValueButBadReturnType()
+        {
+            var data = new CommandData();
+            data.Parameters["test"] = "value";
+            var args = new RunCommandArgs(null, new Dictionary<string, object>(), data, new BuildHelper());
+            Assert.ThrowsException<FormatException>(() => { ExtensionMethods.GetParameter<bool>(args, "test"); });
+        }
+
+        [TestMethod]
+        public void RunCommandArgsGetParameterWithMissingParameterAndNoDefaultValue()
+        {
+            var data = new CommandData();
+            var args = new RunCommandArgs(null, new Dictionary<string, object>(), data, new BuildHelper());
+            Assert.ThrowsException<MissingParameterException>(() => { ExtensionMethods.GetParameter<string>(args, "test"); });
+        }
+
+        [TestMethod]
+        public void RunCommandArgsGetParameterWithMissingArgsAndNoDefaultValue()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() =>
+            {
+                RunCommandArgs args = null;
+                ExtensionMethods.GetParameter<string>(args, "test");
+            });
+        }
+
+        [TestMethod]
+        public void CommandDataGetParameterWithNullArgsAndDefaultValue()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() =>
+            {
+                CommandData args = null;
+                ExtensionMethods.GetParameter<string>(args, "test", string.Empty);
+            });
+        }
+
+        [TestMethod]
+        public void CommandDataGetParameterWithValidArgsAndNoDefaultValue()
+        {
+            var args = new CommandData();
+            args.Parameters["test"] = "value";
+            var value = ExtensionMethods.GetParameter<string>(args, "test");
+            Assert.AreEqual("value", value);
+        }
+
+        [TestMethod]
+        public void CommandDataGetParameterWithValidArgsAndDefaultValue()
+        {
+            var args = new CommandData();
+            var value = ExtensionMethods.GetParameter<string>(args, "test", "default");
+            Assert.AreEqual("default", value);
+        }
+
+        [TestMethod]
+        public void GetValueWithNullArgsAndDefaultValue()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() =>
+            {
+                IDictionary<string, object> dic = null;
+                ExtensionMethods.GetValue<string>(dic, "test", string.Empty);
+            });
+        }
+
+        [TestMethod]
+        public void GetValueWithNullArgsAndNoDefaultValue()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() =>
+            {
+                IDictionary<string, object> dic = null;
+                ExtensionMethods.GetValue<string>(dic, "test");
+            });
+        }
+
+        [TestMethod]
+        public void GetValueWithValidArgsAndNoDefaultValue()
+        {
+            Assert.ThrowsException<KeyNotFoundException>(() =>
+            {
+                var dic = new Dictionary<string, object>();
+                ExtensionMethods.GetValue<string>(dic, "test");
+            });
+        }
+
+        [TestMethod]
+        public void GetValueWithValidArgsAndDefaultValue()
+        {
+            var dic = new Dictionary<string, object>();
+            var value = ExtensionMethods.GetValue<string>(dic, "test", "default");
+            Assert.AreEqual("default", value);
+        }
+
+        [TestMethod]
+        public void GetValueWithValidArgsExistingKeyAndNoDefaultValue()
+        {
+            var dic = new Dictionary<string, object>();
+            dic["test"] = "value";
+            var value = ExtensionMethods.GetValue<string>(dic, "test");
+            Assert.AreEqual("value", value);
+        }
+
+        [TestMethod]
+        public void GetValueWithValidArgsExistingKeyAndNoDefaultValueBadCasting()
+        {
+            var dic = new Dictionary<string, object>();
+            dic["test"] = "value";
+            Assert.ThrowsException<FormatException>(() => { ExtensionMethods.GetValue<bool>(dic, "test"); });
+        }
+
+        [TestMethod]
+        public void GetVariableWithNullArgsAndDefaultValue()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() =>
+            {
+                RunCommandArgs args = null;
+                ExtensionMethods.GetVariable<string>(args, "test", string.Empty);
+            });
+        }
+
+        [TestMethod]
+        public void GetVariableWithNullArgsAndNoDefaultValue()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() =>
+            {
+                RunCommandArgs args = null;
+                ExtensionMethods.GetVariable<string>(args, "test");
+            });
+        }
+
+        [TestMethod]
+        public void GetVariableThatReturnsDefaultValue()
+        {
+            var args = new RunCommandArgs(null, new Dictionary<string, object>(), new CommandData(), new BuildHelper());
+            var value = ExtensionMethods.GetVariable<string>(args, "test", "defaultValue");
+            Assert.AreEqual("defaultValue", value);
+        }
+
+        [TestMethod]
+        public void GetMissingVariableWithoutDefaultValue()
+        {
+            var args = new RunCommandArgs(null, new Dictionary<string, object>(), new CommandData(), new BuildHelper());
+            Assert.ThrowsException<MissingVariableException>(() =>
+            {
+                var value = ExtensionMethods.GetVariable<string>(args, "test");
+            });
+        }
+
+        [TestMethod]
+        public void GetValidVariableWithoutDefaultValue()
+        {
+            var variables = new Dictionary<string, object>();
+            variables["test"] = "someValue";
+            var args = new RunCommandArgs(null, variables, new CommandData(), new BuildHelper());
+
+            var value = ExtensionMethods.GetVariable<string>(args, "test");
+            Assert.AreEqual("someValue", value);
+        }
+
+        [TestMethod]
+        public void GetValidVariableWithBadCastingType()
+        {
+            var variables = new Dictionary<string, object>();
+            variables["test"] = "value";
+            var args = new RunCommandArgs(null, variables, new CommandData(), new BuildHelper());
+            Assert.ThrowsException<FormatException>(() =>
+            {
+                var value = ExtensionMethods.GetVariable<int>(args, "test");
+            });
         }
     }
 }
