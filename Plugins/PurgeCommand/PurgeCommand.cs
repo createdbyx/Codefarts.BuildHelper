0e892253ea290e6d7d75e85d597cf2c90ff12714
Dean <deanlunz@createdbyx.com>
2021-06-07 03:57:32 -0700

    code cleanup

--- Plugins/PurgeCommand/PurgeCommand.cs
+++ Plugins/PurgeCommand/PurgeCommand.cs
@@ -9,7 +9,6 @@ namespace Codefarts.BuildHelper
     using System;
     using System.Collections.Generic;
     using System.IO;
-    using System.Xml;
 
     [NamedParameter("path", typeof(string), true, "The full directory path to be purged.")]
     [NamedParameter("type", typeof(string), true, "The type of items to e purged. 'files' or 'folders'.")]
@@ -22,10 +21,10 @@ namespace Codefarts.BuildHelper
 
         public void Run(RunCommandArgs args)
         {
-            var srcPath = args.GetParameter<string>("path");
+            var srcPath = args.GetParameter<string>("path", null);
             if (srcPath == null)
             {
-                throw new XmlException($"Command: {nameof(PurgeCommand)} value: path  - Value not found");
+                throw new MissingParameterException("path");
             }
 
             srcPath = srcPath.ReplaceVariableStrings(args.Variables);
@@ -33,44 +32,29 @@ namespace Codefarts.BuildHelper
             // var message = args.Element.GetAttributeValue("message");
 
             // check type of purge
-            var typeValue = args.GetParameter<string>("type");
+            var typeValue = args.GetParameter<string>("type", null);
             typeValue = typeValue == null ? typeValue : typeValue.ToLowerInvariant().Trim();
             switch (typeValue)
             {
                 case null:
-                    throw new ArgumentNullException($"'{typeValue}' is missing.");
+                    throw new MissingParameterException("type");
 
                 case "files":
                 case "folders":
                     break;
 
                 default:
-                    throw new ArgumentOutOfRangeException($"'{typeValue}' attribute exists but it value could not be understood.");
+                    throw new BuildException("'type' parameter exists but it value could not be understood.");
             }
 
             // check type of conditions
             var allConditions = args.GetParameter("allconditions", true);
-            //var allConditions = true;
-            //if (conditionsValue != null && !bool.TryParse(conditionsValue, out allConditions))
-            //{
-            //    throw new ArgumentOutOfRangeException($"'{allConditions}' attribute exists but it's value could not be parsed as a bool value.");
-            //}
 
-            // check to use full file paths 
+            // check to use full file paths.
             var fullPaths = args.GetParameter("fullpaths", false);
-            //var fullPaths = false;
-            //if (fullPathsValue != null && !bool.TryParse(fullPathsValue, out fullPaths))
-            //{
-            //    throw new ArgumentOutOfRangeException($"'{fullPaths}' attribute exists but it's value could not be parsed as a bool value.");
-            //}
 
             // check if we should clear subfolders as well
             var subfolders = args.GetParameter("subfolders", true);
-            //var subfolders = true;
-            //if (subFoldersValue != null && !bool.TryParse(subFoldersValue, out subfolders))
-            //{
-            //    throw new ArgumentOutOfRangeException($"'{subfolders}' attribute exists but it's value could not be parsed as a bool value.");
-            //}
 
             var di = new DirectoryInfo(srcPath);
             if (!di.Exists)
@@ -79,41 +63,21 @@ namespace Codefarts.BuildHelper
                 return;
             }
 
-            //if (!string.IsNullOrWhiteSpace(message))
-            //{
-            //    message = message.ReplaceBuildVariableStrings(args.Variables);
-            //    args.Output($"Message: {message}");
-            //}
-
             args.Output($"(Sub Folders->{subfolders}): {srcPath}");
 
-            var getEntries = new Func<string, IEnumerable<string>>(p =>
+            string[] allEntries;
+            var searchOption = subfolders ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+            if (typeValue == "files")
             {
-                if (typeValue == "files")
-                {
-                    return Directory.GetFiles(srcPath, "*.*",
-                        subfolders
-                            ? SearchOption.AllDirectories
-                            : SearchOption.TopDirectoryOnly); //.Select(d => fullPaths ? d : Path.GetFileName(d));
-                }
-
-                return Directory.GetDirectories(srcPath, "*.*",
-                    subfolders ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly); //.Select(d => fullPaths ? d : Path.GetFileName(d));
-            });
-
-            var deleteEntry = new Action<string>(p =>
+                allEntries = Directory.GetFiles(srcPath, "*.*", searchOption);
+            }
+            else
             {
-                if (typeValue == "files")
-                {
-                    File.Delete(p);
-                    return;
-                }
-
-                Directory.Delete(p, true);
-            });
+                allEntries = Directory.GetDirectories(srcPath, "*.*", searchOption);
+            }
 
             // TODO: need option to specify weather or not $(PurgeFile) is just the filename or full path
-            var allEntries = getEntries(srcPath);
+            // var allEntries = getEntries(srcPath);
             var modifiedVars = new Dictionary<string, object>(args.Variables);
             foreach (var file in allEntries)
             {
@@ -124,7 +88,15 @@ namespace Codefarts.BuildHelper
                 {
                     // TODO: need ability to purge folder as well
                     args.Output("Purging: " + file);
-                    deleteEntry(file);
+
+                    if (typeValue == "files")
+                    {
+                        File.Delete(file);
+                    }
+                    else
+                    {
+                        Directory.Delete(file, true);
+                    }
                 }
             }
         }
