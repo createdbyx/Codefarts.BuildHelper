33f704eb8490348985015694ac5f44e5ba4b7d01
Dean <deanlunz@createdbyx.com>
2021-06-08 11:56:53 -0700

    more variable tests

--- BuildHelperTests/PluginTests/VariableTests.cs
+++ BuildHelperTests/PluginTests/VariableTests.cs
@@ -25,21 +25,85 @@ namespace BuildHelperTests
         }
 
         [TestMethod]
-        public void NullArgs()
+        public void AssignVariableNullArgs()
         {
             var command = new AssignVariableCommand();
             Assert.ThrowsException<ArgumentNullException>(() => command.Run(null));
         }
 
         [TestMethod]
-        public void ValidateName()
+        public void RemoveVariableNullArgs()
+        {
+            var command = new RemoveVariableCommand();
+            Assert.ThrowsException<ArgumentNullException>(() => command.Run(null));
+        }
+
+        [TestMethod]
+        public void AssignVariableValidateName()
         {
             var command = new AssignVariableCommand();
             Assert.AreEqual("variable", command.Name);
         }
 
         [TestMethod]
-        public void SetVariable()
+        public void RemoveVariableValidateName()
+        {
+            var command = new RemoveVariableCommand();
+            Assert.AreEqual("removevariable", command.Name);
+        }
+
+        [TestMethod]
+        public void RemoveExistingSetVariable()
+        {
+            var variables = this.SetVariable();
+            var command = new RemoveVariableCommand();
+            var data = new CommandData(command.Name);
+            data.Parameters["name"] = "name";
+            var args = new RunCommandArgs(null, variables, data, new BuildHelper());
+
+            command.Run(args);
+            Assert.IsNotNull(args.Result);
+            Assert.AreEqual(RunStatus.Sucessful, args.Result.Status);
+            Assert.IsTrue(args.Result.GetReturnValue<bool>());
+            Assert.IsFalse(variables.ContainsKey("name"));
+        }
+
+        [TestMethod]
+        public void RemoveVariableMissingNameParameter()
+        {
+            var variables = this.SetVariable();
+            var command = new RemoveVariableCommand();
+            var data = new CommandData(command.Name);
+            var args = new RunCommandArgs(null, variables, data, new BuildHelper());
+
+            command.Run(args);
+            Assert.IsNotNull(args.Result);
+            Assert.AreEqual(RunStatus.Errored, args.Result.Status);
+            Assert.AreSame(typeof(MissingParameterException), args.Result.Error.GetType());
+
+            var mpe = args.Result.Error as MissingParameterException;
+            Assert.AreEqual("name", mpe.ParameterName);
+        }
+
+        [TestMethod]
+        public void RemoveNonExistingSetVariable()
+        {
+            var variables = this.SetVariable();
+            var command = new RemoveVariableCommand();
+            var data = new CommandData(command.Name);
+            data.Parameters["name"] = "notExisting";
+            var args = new RunCommandArgs(null, variables, data, new BuildHelper());
+
+            command.Run(args);
+            Assert.IsNotNull(args.Result);
+            Assert.AreEqual(RunStatus.Sucessful, args.Result.Status);
+            Assert.IsFalse(args.Result.GetReturnValue<bool>());
+            Assert.IsTrue(variables.ContainsKey("name"));
+            Assert.AreEqual(1, variables.Count);
+        }
+
+        [TestMethod]
+        public IDictionary<string, object> SetVariable()
         {
             var command = new AssignVariableCommand();
             var data = new CommandData();
@@ -53,6 +117,8 @@ namespace BuildHelperTests
             Assert.AreEqual(RunStatus.Sucessful, args.Result.Status);
             Assert.IsTrue(variables.ContainsKey("name"));
             Assert.AreEqual("value", variables["name"]);
+
+            return variables;
         }
 
         [TestMethod]
