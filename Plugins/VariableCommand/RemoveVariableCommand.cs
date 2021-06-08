16410f4344ec207a7ebd509cac763bbd1a7b6778
Dean <deanlunz@createdbyx.com>
2021-06-08 11:55:32 -0700

    modified to return a result

--- Plugins/VariableCommand/RemoveVariableCommand.cs
+++ Plugins/VariableCommand/RemoveVariableCommand.cs
@@ -6,7 +6,7 @@
 
 namespace Codefarts.BuildHelper
 {
-    using System.Xml;
+    using System;
 
     [NamedParameter("name", typeof(string), true, "The name of the variable.")]
     public class RemoveVariableCommand : ICommandPlugin
@@ -15,18 +15,25 @@ namespace Codefarts.BuildHelper
 
         public void Run(RunCommandArgs args)
         {
+            if (args == null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+
             // get variable name
-            var nameValue = args.GetParameter<string>("name");
+            var nameValue = args.GetParameter<string>("name", null);
             if (nameValue == null)
             {
-                throw new XmlException($"Command: {nameof(RemoveVariableCommand)} value: name - Value not found");
+                args.Result = RunResult.Errored(new MissingParameterException("name"));
+                return;
             }
 
             // replace variable strings
             nameValue = nameValue.ReplaceVariableStrings(args.Variables);
 
             // remove the variable and report result
-            if (args.Variables.Remove(nameValue))
+            var wasRemoved = args.Variables.Remove(nameValue);
+            if (wasRemoved)
             {
                 args.Output($"Variable Removed: {nameValue}");
             }
@@ -34,6 +41,8 @@ namespace Codefarts.BuildHelper
             {
                 args.Output($"Variable NOT Removed: {nameValue}");
             }
+
+            args.Result = RunResult.Sucessful(wasRemoved);
         }
     }
 }
