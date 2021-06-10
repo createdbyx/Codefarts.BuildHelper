610a6be494008ea347ccfdd72211d82228bc06ee
Dean <deanlunz@createdbyx.com>
2021-06-10 03:31:06 -0700

    added more sample data projects added more unit tests and with full test coverage

--- BuildHelperTests/PluginTests/PurgeFiles.cs
+++ BuildHelperTests/PluginTests/PurgeFiles.cs
@@ -24,13 +24,13 @@ namespace BuildHelperTests
         {
             this.tempDir = Path.Combine(Path.GetTempPath(), "PurgeFilesTest_" + Guid.NewGuid().ToString("N"));
             Directory.CreateDirectory(this.tempDir);
-            File.WriteAllText(Path.Combine(this.tempDir, "File1.txt"), "File1Data");
-            File.WriteAllText(Path.Combine(this.tempDir, "File2.xml"), "File2Data");
-            File.WriteAllText(Path.Combine(this.tempDir, "System.File3.dat"), "File3Data");
-            Directory.CreateDirectory(Path.Combine(this.tempDir, "SubFolder"));
-            Directory.CreateDirectory(Path.Combine(this.tempDir, "SubFolder", "Sub2"));
-            File.WriteAllText(Path.Combine(this.tempDir, "SubFolder", "Microsoft.File4.db"), "File4Data");
-            File.WriteAllText(Path.Combine(this.tempDir, "SubFolder", "Sub2", "Taxi.File5.pdb"), "File5Data");
+            File.WriteAllText(Path.Combine(this.tempDir, "File1.txt"), "File1Data"); // \File1.txt
+            File.WriteAllText(Path.Combine(this.tempDir, "File2.xml"), "File2Data"); // \File2.xml
+            File.WriteAllText(Path.Combine(this.tempDir, "System.File3.dat"), "File3Data"); // \System.File3.dat
+            Directory.CreateDirectory(Path.Combine(this.tempDir, "SubFolder")); // \SubFolder\
+            Directory.CreateDirectory(Path.Combine(this.tempDir, "SubFolder", "Sub2")); // \SubFolder\Sub2\
+            File.WriteAllText(Path.Combine(this.tempDir, "SubFolder", "Microsoft.File4.db"), "File4Data"); // \SubFolder\Microsoft.File4.db
+            File.WriteAllText(Path.Combine(this.tempDir, "SubFolder", "Sub2", "Taxi.File5.pdb"), "File5Data"); // \SubFolder\Sub2\Taxi.File5.pdb
             this.variables = new Dictionary<string, object>();
             this.variables["TempPath"] = this.tempDir;
         }
@@ -40,6 +40,217 @@ namespace BuildHelperTests
         {
             Directory.Delete(tempDir, true);
             this.variables = null;
+        }
+
+        [TestMethod]
+        public void ValidateCommandName()
+        {
+            var purge = new PurgeCommand();
+            Assert.AreEqual("purge", purge.Name);
+        }
+
+        [TestMethod]
+        public void NullArgs()
+        {
+            var purge = new PurgeCommand();
+            Assert.ThrowsException<ArgumentNullException>(() => purge.Run(null));
+        }
+
+        [TestMethod]
+        public void TryPurgeReadOnlyFile()
+        {
+            var purge = new PurgeCommand();
+            var data =
+                "<purge ignoreconditions=\"true\" path=\"$(TempPath)\" subfolders=\"true\" allconditions=\"false\" fullpaths=\"false\" type=\"files\">\r\n" +
+                "    <condition value1=\"$(PurgeEntry)\" operator=\"startswith\" value2=\"System.\" ignorecase=\"true\" />\r\n" +
+                "    <condition value1=\"$(PurgeEntry)\" operator=\"startswith\" value2=\"Microsoft.\" ignorecase=\"true\" />\r\n" +
+                "    <condition value1=\"$(PurgeEntry)\" operator=\"endswith\" value2=\".xml\" ignorecase=\"true\" />\r\n" +
+                "</purge>";
+
+            var item = XElement.Parse(data);
+            var buildFileCommand = TestHelpers.BuildCommandNode(item, null);
+            var args = new RunCommandArgs(msg => { }, this.variables, buildFileCommand, new BuildHelper());
+
+            // make first file readonly
+            var firstFile = new FileInfo(Path.Combine(this.tempDir, "File1.txt"));
+            firstFile.IsReadOnly = true;
+
+            purge.Run(args);
+
+            Assert.IsNotNull(args.Result);
+            Assert.AreEqual(RunStatus.Errored, args.Result.Status);
+            Assert.IsNotNull(args.Result.Error);
+
+            var uae = args.Result.Error as UnauthorizedAccessException;
+            Assert.IsNotNull(uae);
+
+            // remove readonly so cleanup can occur
+            firstFile.IsReadOnly = false;
+        }
+
+        [TestMethod]
+        public void IgnoreConditionsSetTrue()
+        {
+            var purge = new PurgeCommand();
+            var data =
+                "<purge ignoreconditions=\"true\" path=\"$(TempPath)\" subfolders=\"true\" allconditions=\"false\" fullpaths=\"false\" type=\"files\">\r\n" +
+                "    <condition value1=\"$(PurgeEntry)\" operator=\"startswith\" value2=\"System.\" ignorecase=\"true\" />\r\n" +
+                "    <condition value1=\"$(PurgeEntry)\" operator=\"startswith\" value2=\"Microsoft.\" ignorecase=\"true\" />\r\n" +
+                "    <condition value1=\"$(PurgeEntry)\" operator=\"endswith\" value2=\".xml\" ignorecase=\"true\" />\r\n" +
+                "</purge>";
+
+            var item = XElement.Parse(data);
+            var buildFileCommand = TestHelpers.BuildCommandNode(item, null);
+            purge.Run(new RunCommandArgs(msg => { }, this.variables, buildFileCommand, new BuildHelper()));
+
+            var files = Directory.GetFiles(this.tempDir, "*.*", SearchOption.AllDirectories);
+            var folders = Directory.GetDirectories(this.tempDir, "*.*", SearchOption.AllDirectories);
+            Assert.AreEqual(0, files.Length);
+            Assert.AreEqual(2, folders.Length);
+        }
+
+        [TestMethod]
+        public void MissingPathParameter()
+        {
+            var purge = new PurgeCommand();
+            var data =
+                "<purge subfolders=\"true\" allconditions=\"false\" fullpaths=\"false\" type=\"files\">\r\n" +
+                "    <condition value1=\"$(PurgeEntry)\" operator=\"startswith\" value2=\"System.\" ignorecase=\"true\" />\r\n" +
+                "    <condition value1=\"$(PurgeEntry)\" operator=\"startswith\" value2=\"Microsoft.\" ignorecase=\"true\" />\r\n" +
+                "    <condition value1=\"$(PurgeEntry)\" operator=\"endswith\" value2=\".xml\" ignorecase=\"true\" />\r\n" +
+                "</purge>";
+
+            var item = XElement.Parse(data);
+            var buildFileCommand = TestHelpers.BuildCommandNode(item, null);
+            var args = new RunCommandArgs(null, this.variables, buildFileCommand, new BuildHelper());
+            purge.Run(args);
+
+            Assert.IsNotNull(args.Result);
+            Assert.AreEqual(RunStatus.Errored, args.Result.Status);
+            Assert.AreSame(typeof(MissingParameterException), args.Result.Error.GetType());
+
+            var mpe = args.Result.Error as MissingParameterException;
+            Assert.AreEqual("path", mpe.ParameterName);
+        }
+
+        [TestMethod]
+        public void MissingTypeParameter()
+        {
+            var purge = new PurgeCommand();
+            var data =
+                "<purge subfolders=\"true\" path=\"$(TempPath)\" allconditions=\"false\" fullpaths=\"false\" >\r\n" +
+                "    <condition value1=\"$(PurgeEntry)\" operator=\"startswith\" value2=\"System.\" ignorecase=\"true\" />\r\n" +
+                "    <condition value1=\"$(PurgeEntry)\" operator=\"startswith\" value2=\"Microsoft.\" ignorecase=\"true\" />\r\n" +
+                "    <condition value1=\"$(PurgeEntry)\" operator=\"endswith\" value2=\".xml\" ignorecase=\"true\" />\r\n" +
+                "</purge>";
+
+            var item = XElement.Parse(data);
+            var buildFileCommand = TestHelpers.BuildCommandNode(item, null);
+            var args = new RunCommandArgs(null, this.variables, buildFileCommand, new BuildHelper());
+            purge.Run(args);
+
+            Assert.IsNotNull(args.Result);
+            Assert.AreEqual(RunStatus.Errored, args.Result.Status);
+            Assert.AreSame(typeof(MissingParameterException), args.Result.Error.GetType());
+
+            var mpe = args.Result.Error as MissingParameterException;
+            Assert.AreEqual("type", mpe.ParameterName);
+        }
+
+        [TestMethod]
+        public void PathParameterNoValue()
+        {
+            var purge = new PurgeCommand();
+            var data =
+                "<purge subfolders=\"true\" path=\"\" allconditions=\"false\" fullpaths=\"false\" type=\"files\">\r\n" +
+                "    <condition value1=\"$(PurgeEntry)\" operator=\"startswith\" value2=\"System.\" ignorecase=\"true\" />\r\n" +
+                "    <condition value1=\"$(PurgeEntry)\" operator=\"startswith\" value2=\"Microsoft.\" ignorecase=\"true\" />\r\n" +
+                "    <condition value1=\"$(PurgeEntry)\" operator=\"endswith\" value2=\".xml\" ignorecase=\"true\" />\r\n" +
+                "</purge>";
+
+            var item = XElement.Parse(data);
+            var buildFileCommand = TestHelpers.BuildCommandNode(item, null);
+            var args = new RunCommandArgs(null, this.variables, buildFileCommand, new BuildHelper());
+            purge.Run(args);
+
+            Assert.IsNotNull(args.Result);
+            Assert.AreEqual(RunStatus.Errored, args.Result.Status);
+            Assert.AreSame(typeof(MissingParameterException), args.Result.Error.GetType());
+
+            var mpe = args.Result.Error as MissingParameterException;
+            Assert.AreEqual("path", mpe.ParameterName);
+        }
+
+        [TestMethod]
+        public void TypeParameterNoValue()
+        {
+            var purge = new PurgeCommand();
+            var data =
+                "<purge subfolders=\"true\" path=\"$(TempPath)\" allconditions=\"false\" fullpaths=\"false\" type=\"\">\r\n" +
+                "    <condition value1=\"$(PurgeEntry)\" operator=\"startswith\" value2=\"System.\" ignorecase=\"true\" />\r\n" +
+                "    <condition value1=\"$(PurgeEntry)\" operator=\"startswith\" value2=\"Microsoft.\" ignorecase=\"true\" />\r\n" +
+                "    <condition value1=\"$(PurgeEntry)\" operator=\"endswith\" value2=\".xml\" ignorecase=\"true\" />\r\n" +
+                "</purge>";
+
+            var item = XElement.Parse(data);
+            var buildFileCommand = TestHelpers.BuildCommandNode(item, null);
+            var args = new RunCommandArgs(null, this.variables, buildFileCommand, new BuildHelper());
+            purge.Run(args);
+
+            Assert.IsNotNull(args.Result);
+            Assert.AreEqual(RunStatus.Errored, args.Result.Status);
+            Assert.AreSame(typeof(BuildException), args.Result.Error.GetType());
+
+            var buildException = args.Result.Error as BuildException;
+            Assert.IsNotNull(buildException);
+        }
+
+        [TestMethod]
+        public void TypeParameterBadValue()
+        {
+            var purge = new PurgeCommand();
+            var data =
+                "<purge subfolders=\"true\" path=\"$(TempPath)\" allconditions=\"false\" fullpaths=\"false\" type=\"badValue\">\r\n" +
+                "    <condition value1=\"$(PurgeEntry)\" operator=\"startswith\" value2=\"System.\" ignorecase=\"true\" />\r\n" +
+                "    <condition value1=\"$(PurgeEntry)\" operator=\"startswith\" value2=\"Microsoft.\" ignorecase=\"true\" />\r\n" +
+                "    <condition value1=\"$(PurgeEntry)\" operator=\"endswith\" value2=\".xml\" ignorecase=\"true\" />\r\n" +
+                "</purge>";
+
+            var item = XElement.Parse(data);
+            var buildFileCommand = TestHelpers.BuildCommandNode(item, null);
+            var args = new RunCommandArgs(null, this.variables, buildFileCommand, new BuildHelper());
+            purge.Run(args);
+
+            Assert.IsNotNull(args.Result);
+            Assert.AreEqual(RunStatus.Errored, args.Result.Status);
+            Assert.AreSame(typeof(BuildException), args.Result.Error.GetType());
+
+            var mpe = args.Result.Error as BuildException;
+            Assert.IsNotNull(mpe);
+        }
+
+        [TestMethod]
+        public void PathParameterPathDoesNotExist()
+        {
+            var purge = new PurgeCommand();
+            var data =
+                "<purge subfolders=\"true\" path=\"z:\\someNonExistent\\Path\" allconditions=\"false\" fullpaths=\"false\" type=\"files\">\r\n" +
+                "    <condition value1=\"$(PurgeEntry)\" operator=\"startswith\" value2=\"System.\" ignorecase=\"true\" />\r\n" +
+                "    <condition value1=\"$(PurgeEntry)\" operator=\"startswith\" value2=\"Microsoft.\" ignorecase=\"true\" />\r\n" +
+                "    <condition value1=\"$(PurgeEntry)\" operator=\"endswith\" value2=\".xml\" ignorecase=\"true\" />\r\n" +
+                "</purge>";
+
+            var item = XElement.Parse(data);
+            var buildFileCommand = TestHelpers.BuildCommandNode(item, null);
+            var args = new RunCommandArgs(null, this.variables, buildFileCommand, new BuildHelper());
+            purge.Run(args);
+
+            Assert.IsNotNull(args.Result);
+            Assert.AreEqual(RunStatus.Errored, args.Result.Status);
+            Assert.AreSame(typeof(DirectoryNotFoundException), args.Result.Error.GetType());
+
+            var dnf = args.Result.Error as DirectoryNotFoundException;
+            Assert.IsNotNull(dnf);
         }
 
         [TestMethod]
--- BuildHelperTests/PluginTests/VersionUpdaterTests.cs
+++ BuildHelperTests/PluginTests/VersionUpdaterTests.cs
@@ -19,6 +19,9 @@ namespace BuildHelperTests
         private const string CommandName = "updateversion";
         private string tempFolder;
         private string sampleProjectPath;
+        private string sampleProjectPathFileBadLengths;
+        private string sampleProjectPathAssemblyBadLengths;
+        private string sampleProjectPathNonIntegerFileVersionRevision;
 
         [TestInitialize]
         public void InitTest()
@@ -26,8 +29,21 @@ namespace BuildHelperTests
             this.tempFolder = Path.Combine(Path.GetTempPath(), "VersionTests_" + Guid.NewGuid().ToString("N"));
             Directory.CreateDirectory(this.tempFolder);
             this.sampleProjectPath = Path.Combine(this.tempFolder, "SampleProject.csproj");
+            this.sampleProjectPathFileBadLengths = Path.Combine(this.tempFolder, "SampleProjectBadFileVersionLength.csproj");
+            this.sampleProjectPathAssemblyBadLengths = Path.Combine(this.tempFolder, "SampleProjectBadAssemblyVersionLength.csproj");
+            this.sampleProjectPathNonIntegerFileVersionRevision = Path.Combine(this.tempFolder, "SampleProjectNonIntegerFileVersionRevision.csproj");
+
             var sourceFileName = Path.Combine(Environment.CurrentDirectory, "SampleData", "SampleProject.csproj");
             File.Copy(sourceFileName, this.sampleProjectPath);
+
+            sourceFileName = Path.Combine(Environment.CurrentDirectory, "SampleData", "SampleProjectBadFileVersionLength.csproj");
+            File.Copy(sourceFileName, this.sampleProjectPathFileBadLengths);
+
+            sourceFileName = Path.Combine(Environment.CurrentDirectory, "SampleData", "SampleProjectBadAssemblyVersionLength.csproj");
+            File.Copy(sourceFileName, this.sampleProjectPathAssemblyBadLengths);
+
+            sourceFileName = Path.Combine(Environment.CurrentDirectory, "SampleData", "SampleProjectNonIntegerFileVersionRevision.csproj");
+            File.Copy(sourceFileName, this.sampleProjectPathNonIntegerFileVersionRevision);
         }
 
         [TestCleanup]
@@ -40,26 +56,140 @@ namespace BuildHelperTests
         }
 
         [TestMethod]
+        public void ValidateName()
+        {
+            var command = new VersionUpdaterCommand();
+            Assert.AreEqual("updateversion", command.Name);
+        }
+
+        [TestMethod]
+        public void NullArgs()
+        {
+            var command = new VersionUpdaterCommand();
+            Assert.ThrowsException<ArgumentNullException>(() => command.Run(null));
+        }
+
+        [TestMethod]
         public void DefaultNoParameters()
         {
-            var date = DateTime.Now;
             var command = new VersionUpdaterCommand();
             var vars = new Dictionary<string, object>();
-            vars["ProjectFileName"] = this.sampleProjectPath;
-
             var parameters = new Dictionary<string, object>();
-
             var cmdNode = new CommandData(CommandName, parameters);
             var args = new RunCommandArgs(null, vars, cmdNode, new BuildHelper());
 
             // run
             command.Run(args);
 
-            // validate data
-            var fileData = File.ReadAllText(this.sampleProjectPath);
+            Assert.IsNotNull(args.Result);
+            Assert.AreEqual(RunStatus.Errored, args.Result.Status);
+            Assert.IsNotNull(args.Result.Error);
+            Assert.AreSame(typeof(MissingParameterException), args.Result.Error.GetType());
+            var mpe = args.Result.Error as MissingParameterException;
+            Assert.AreEqual("ProjectFileName", mpe.ParameterName);
+        }
 
-            Assert.IsTrue(fileData.Contains($"<AssemblyVersion>{date.Year}.{date.Month}.{date.Day}.1</AssemblyVersion>"));
-            Assert.IsTrue(fileData.Contains($"<FileVersion>{date.Year}.{date.Month}.{date.Day}.1</FileVersion>"));
+        [TestMethod]
+        public void ProjectFileLockedNoSharing()
+        {
+            var command = new VersionUpdaterCommand();
+            var vars = new Dictionary<string, object>();
+            var parameters = new Dictionary<string, object>();
+            parameters["ProjectFileName"] = this.sampleProjectPath;
+            var cmdNode = new CommandData(CommandName, parameters);
+            var args = new RunCommandArgs(null, vars, cmdNode, new BuildHelper());
+
+            // lock file
+            using (var fs = new FileStream(this.sampleProjectPath, FileMode.Open, FileAccess.Read, FileShare.None))
+            {
+                // run
+                command.Run(args);
+            }
+
+            Assert.IsNotNull(args.Result);
+            Assert.AreEqual(RunStatus.Errored, args.Result.Status);
+            Assert.IsNotNull(args.Result.Error);
+            Assert.AreSame(typeof(IOException), args.Result.Error.GetType());
+        }
+
+        [TestMethod]
+        public void ProjectFileLockedReadOnlySharing()
+        {
+            var command = new VersionUpdaterCommand();
+            var vars = new Dictionary<string, object>();
+            var parameters = new Dictionary<string, object>();
+            parameters["ProjectFileName"] = this.sampleProjectPath;
+            var cmdNode = new CommandData(CommandName, parameters);
+            var args = new RunCommandArgs(null, vars, cmdNode, new BuildHelper());
+
+            // lock file
+            using (var fs = new FileStream(this.sampleProjectPath, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                // run
+                command.Run(args);
+            }
+
+            Assert.IsNotNull(args.Result);
+            Assert.AreEqual(RunStatus.Errored, args.Result.Status);
+            Assert.IsNotNull(args.Result.Error);
+            Assert.AreSame(typeof(IOException), args.Result.Error.GetType());
+        }
+
+        [TestMethod]
+        public void ProjectFileVersionBadFileLength()
+        {
+            var command = new VersionUpdaterCommand();
+            var vars = new Dictionary<string, object>();
+            var parameters = new Dictionary<string, object>();
+            parameters["ProjectFileName"] = this.sampleProjectPathFileBadLengths;
+            var cmdNode = new CommandData(CommandName, parameters);
+            var args = new RunCommandArgs(null, vars, cmdNode, new BuildHelper());
+
+            // run
+            command.Run(args);
+
+            Assert.IsNotNull(args.Result);
+            Assert.AreEqual(RunStatus.Errored, args.Result.Status);
+            Assert.IsNotNull(args.Result.Error);
+            Assert.AreSame(typeof(BuildException), args.Result.Error.GetType());
+        }
+
+        [TestMethod]
+        public void ProjectFileVersionBadAssemblyLength()
+        {
+            var command = new VersionUpdaterCommand();
+            var vars = new Dictionary<string, object>();
+            var parameters = new Dictionary<string, object>();
+            parameters["ProjectFileName"] = this.sampleProjectPathAssemblyBadLengths;
+            var cmdNode = new CommandData(CommandName, parameters);
+            var args = new RunCommandArgs(null, vars, cmdNode, new BuildHelper());
+
+            // run
+            command.Run(args);
+
+            Assert.IsNotNull(args.Result);
+            Assert.AreEqual(RunStatus.Errored, args.Result.Status);
+            Assert.IsNotNull(args.Result.Error);
+            Assert.AreSame(typeof(BuildException), args.Result.Error.GetType());
+        }
+
+        [TestMethod]
+        public void ProjectFileNonIntegerFileRevision()
+        {
+            var command = new VersionUpdaterCommand();
+            var vars = new Dictionary<string, object>();
+            var parameters = new Dictionary<string, object>();
+            parameters["ProjectFileName"] = this.sampleProjectPathNonIntegerFileVersionRevision;
+            var cmdNode = new CommandData(CommandName, parameters);
+            var args = new RunCommandArgs(null, vars, cmdNode, new BuildHelper());
+
+            // run
+            command.Run(args);
+
+            Assert.IsNotNull(args.Result);
+            Assert.AreEqual(RunStatus.Errored, args.Result.Status);
+            Assert.IsNotNull(args.Result.Error);
+            Assert.AreSame(typeof(BuildException), args.Result.Error.GetType());
         }
 
         [TestMethod]
@@ -68,10 +198,10 @@ namespace BuildHelperTests
             var date = DateTime.Now;
             var command = new VersionUpdaterCommand();
             var vars = new Dictionary<string, object>();
-            vars["ProjectFileName"] = this.sampleProjectPath;
-
             var parameters = new Dictionary<string, object>();
             parameters["file"] = true;
+            parameters["ProjectFileName"] = this.sampleProjectPath;
+
 
             var cmdNode = new CommandData(CommandName, parameters);
             var args = new RunCommandArgs(null, vars, cmdNode, new BuildHelper());
@@ -92,10 +222,9 @@ namespace BuildHelperTests
             var date = DateTime.Now;
             var command = new VersionUpdaterCommand();
             var vars = new Dictionary<string, object>();
-            vars["ProjectFileName"] = this.sampleProjectPath;
-
             var parameters = new Dictionary<string, object>();
             parameters["assembly"] = true;
+            parameters["ProjectFileName"] = this.sampleProjectPath;
 
             var cmdNode = new CommandData(CommandName, parameters);
             var args = new RunCommandArgs(null, vars, cmdNode, new BuildHelper());
@@ -116,10 +245,9 @@ namespace BuildHelperTests
             var date = DateTime.Now;
             var command = new VersionUpdaterCommand();
             var vars = new Dictionary<string, object>();
-            vars["ProjectFileName"] = this.sampleProjectPath;
-
             var parameters = new Dictionary<string, object>();
             parameters["file"] = false;
+            parameters["ProjectFileName"] = this.sampleProjectPath;
 
             var cmdNode = new CommandData(CommandName, parameters);
             var args = new RunCommandArgs(null, vars, cmdNode, new BuildHelper());
@@ -140,10 +268,9 @@ namespace BuildHelperTests
             var date = DateTime.Now;
             var command = new VersionUpdaterCommand();
             var vars = new Dictionary<string, object>();
-            vars["ProjectFileName"] = this.sampleProjectPath;
-
             var parameters = new Dictionary<string, object>();
             parameters["assembly"] = false;
+            parameters["ProjectFileName"] = this.sampleProjectPath;
 
             var cmdNode = new CommandData(CommandName, parameters);
             var args = new RunCommandArgs(null, vars, cmdNode, new BuildHelper());
