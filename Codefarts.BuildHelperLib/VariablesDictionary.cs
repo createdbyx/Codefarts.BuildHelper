8ed695263ef02e20804833f6d780a5743d6021e7
Dean <deanlunz@createdbyx.com>
2021-07-01 02:50:19 -0700

    added constructors

--- Codefarts.BuildHelperLib/VariablesDictionary.cs
+++ Codefarts.BuildHelperLib/VariablesDictionary.cs
@@ -7,11 +7,27 @@
 namespace Codefarts.BuildHelper
 {
     using System.Collections.Concurrent;
+    using System.Collections.Generic;
 
     /// <summary>
     /// Provides a thread safe dictionary for storing variables.
     /// </summary>
     public class VariablesDictionary : ConcurrentDictionary<string, object>
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VariablesDictionary"/> class.
+        /// </summary>
+        public VariablesDictionary()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VariablesDictionary"/> class.
+        /// </summary>
+        /// <param name="collection">The collection to copy entries from.</param>
+        public VariablesDictionary(IEnumerable<KeyValuePair<string, object>> collection) 
+            : base(collection)
+        {
+        }
     }
 }
