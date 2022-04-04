a554bf7852c30c5cb5292348e092f5a7a323e366
Dean <deanlunz@createdbyx.com>
2022-04-04 03:21:46 -0700

    wrote a deployment powerscript file

---
+++ Codefarts.BuildHelperConsoleApp/Deploy.ps1
@@ -0,0 +1,7 @@
+﻿echo starting
+p:
+cd "\Code Projects\Codefarts.BuildHelper\Codefarts.BuildHelperConsoleApp\bin\Debug\net5.0"
+Get-ChildItem -Include ref -Recurse -Force | Remove-Item -Force -Recurse
+attrib -r "E:\BuildHelper\\*.*" /s
+rmdir /s /q  "E:\BuildHelper\"
+xcopy "P:\Code Projects\Codefarts.BuildHelper\Codefarts.BuildHelperConsoleApp\bin\Debug\net5.0\*.*" "E:\BuildHelper\" /e /y
