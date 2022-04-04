e7243f95f73a00e446230d0a236ad032367f769f
Dean <deanlunz@createdbyx.com>
2022-04-04 03:18:24 -0700

    updated deploy batch file

--- Codefarts.BuildHelperConsoleApp/Deploy.bat
+++ Codefarts.BuildHelperConsoleApp/Deploy.bat
@@ -1,2 +1,4 @@
-﻿rmdir /s  "E:\BuildHelper\"
+﻿echo starting
+attrib -r "E:\BuildHelper\\*.*" /s
+rmdir /s /q  "E:\BuildHelper\"
 xcopy "P:\Code Projects\Codefarts.BuildHelper\Codefarts.BuildHelperConsoleApp\bin\Debug\net5.0\*.*" "E:\BuildHelper\" /e /y
