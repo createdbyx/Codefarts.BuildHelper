639bb5a8f9c5eba687dc22c353b454c3847196ca
Dean <deanlunz@createdbyx.com>
2023-01-13 13:23:36 -0800

    Added more console application tests

--- BuildHelperTests/ConsoleAppTests.cs
+++ BuildHelperTests/ConsoleAppTests.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.IO;
+using BuildHelperTests.Mocks;
+using Codefarts.BuildHelper;
 using Codefarts.BuildHelperConsoleApp;
+using Codefarts.DependencyInjection;
 using Codefarts.IoC;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -24,5 +28,58 @@ public class ConsoleAppTests
         var ioc = new DependencyInjectorShim(new Container());
         var app = new Application(ioc);
         Assert.IsNotNull(app);
+    }
+
+    [TestMethod]
+    public void Run_ValidArguments_NoImporter()
+    {
+        var ioc = new DependencyInjectorShim(new Container());
+        var app = new Application(ioc);
+        Assert.ThrowsException<ContainerResolutionException>(() => app.Run());
+    }
+
+    [TestMethod]
+    public void Run_ValidArguments_WithImporter_Succesful()
+    {
+        var ioc = new DependencyInjectorShim(new Container());
+        ioc.Register<IDependencyInjectionProvider>(() => ioc);
+        var app = new Application(ioc);
+        ioc.Register<ICommandImporter, MockSuccsesfulCommandImporter>();
+
+        var result = app.Run();
+        Assert.IsNotNull(result);
+        Assert.IsNotNull(result.Error);
+        Assert.IsNull(result.ReturnValue);
+    }
+
+    [TestMethod]
+    public void PluginLoader_Ctor_NullArguments()
+    {
+        Assert.ThrowsException<ArgumentNullException>(() =>
+        {
+            var loader = new PluginLoader(null);
+        });
+    }
+
+    [TestMethod]
+    public void PluginLoader_Ctor_ValidArguments()
+    {
+        var ioc = new DependencyInjectorShim(new Container());
+        var loader = new PluginLoader(ioc);
+        Assert.IsNotNull(loader);
+    }
+
+    [TestMethod]
+    [TestCategory("PluginLoader")]
+    public void PluginLoader_MissingPluginFolder()
+    {
+        var ioc = new DependencyInjectorShim(new Container());
+        ioc.Register<IDependencyInjectionProvider>(() => ioc);
+        var loader = new PluginLoader(ioc);
+        var pluginPath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N"));
+        loader.PluginFolder = pluginPath;
+        var plugins = loader.Load();
+        Assert.IsNotNull(plugins);
+        Assert.AreEqual(0, plugins.Count);
     }
 }
