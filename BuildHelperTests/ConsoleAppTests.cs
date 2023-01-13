da4a0fe0ced2725765e3547621f1b76bca9c7a88
Dean <deanlunz@createdbyx.com>
2023-01-13 03:59:35 -0800

    Started adding unit tests for the application class in the main console app

---
+++ BuildHelperTests/ConsoleAppTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Codefarts.BuildHelperConsoleApp;
+using Codefarts.IoC;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BuildHelperTests;
+
+[TestClass]
+[TestCategory("Console Application")]
+public class ConsoleAppTests
+{
+    [TestMethod]
+    public void Ctor_NullArguments()
+    {
+        Assert.ThrowsException<ArgumentNullException>(() =>
+        {
+            var app = new Application(null);
+        });
+    }
+
+    [TestMethod]
+    public void Ctor_ValidArguments()
+    {
+        var ioc = new DependencyInjectorShim(new Container());
+        var app = new Application(ioc);
+        Assert.IsNotNull(app);
+    }
+}
