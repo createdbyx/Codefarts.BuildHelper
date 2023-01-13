ee4e54043125aa4225c5f2ae8444101b96c563a8
Dean <deanlunz@createdbyx.com>
2023-01-13 02:23:42 -0800

    moved some code around and made some tweaks to reading xml build files

--- Codefarts.BuildHelperConsoleApp/Program.cs
+++ Codefarts.BuildHelperConsoleApp/Program.cs
@@ -4,93 +4,68 @@
 // http://www.codefarts.com
 // </copyright>
 
-namespace Codefarts.BuildHelperConsoleApp
+using System.Collections.Generic;
+
+namespace Codefarts.BuildHelperConsoleApp;
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Codefarts.BuildHelper;
+using Codefarts.DependencyInjection;
+using Codefarts.IoC;
+using Codefats.BuildHelper.ConsoleReporter;
+
+static class Program
 {
-    using System;
-    using System.Diagnostics;
-    using System.IO;
-    using System.Linq;
-    using System.Xml.Linq;
-    using Codefarts.BuildHelper;
-    using Codefarts.DependencyInjection;
-    using Codefarts.IoC;
-    using Codefats.BuildHelper.ConsoleReporter;
-
-    static class Program
+    private static void Main(string[] args)
     {
-        private static void Main(string[] args)
+        var ioc = new DependencyInjectorShim(new Container());
+        ioc.Register<IDependencyInjectionProvider>(() => ioc);
+        ioc.Register<IStatusReporter, ConsoleStatusReporter>();
+
+        var status = ioc.Resolve<IStatusReporter>();
+
+        var buildFile = args.FirstOrDefault(x => x.StartsWith("-b:"));
+        buildFile = string.IsNullOrWhiteSpace(buildFile) ? null : buildFile.Substring(3);
+
+        // load command plugins
+        var pluginLoader = ioc.Resolve<PluginLoader>();
+        var appPath = Process.GetCurrentProcess().MainModule.FileName;
+        var appDir = Path.GetDirectoryName(appPath);
+        var pluginFolder = Path.Combine(appDir, "Plugins");
+
+        pluginLoader.PluginFolder = pluginFolder;
+        var commandPlugins = pluginLoader.Load();
+
+        // read build file
+        var variables = new VariablesDictionary();
+        variables["Application"] = Path.GetFileName(appPath);
+
+        IEnumerable<CommandData> buildFileCommands;
+        var buildFileReader = ioc.Resolve<XmlBuildFileReader>();
+        if (!buildFileReader.TryReadBuildFile(buildFile, out buildFileCommands))
         {
-            var ioc = new DependencyInjectorShim(new Container());
-            ioc.Register<IDependencyInjectionProvider>(() => ioc);
-            ioc.Register<IStatusReporter, ConsoleStatusReporter>();
-
-            var status = ioc.Resolve<IStatusReporter>();
-
-            var buildFile = args.FirstOrDefault(x => x.StartsWith("-b:"));
-            buildFile = string.IsNullOrWhiteSpace(buildFile) ? null : buildFile.Substring(3);
-
-            // load command plugins
-            var pluginLoader = ioc.Resolve<PluginLoader>();
-            var appPath = Process.GetCurrentProcess().MainModule.FileName;
-            var appDir = Path.GetDirectoryName(appPath);
-            var pluginFolder = Path.Combine(appDir, "Plugins");
-
-            pluginLoader.PluginFolder = pluginFolder;
-            var commandPlugins = pluginLoader.Load();
-
-            // read build file
-            var variables = new VariablesDictionary();
-            variables["Application"] = Path.GetFileName(appPath);
-
-            XElement root;
-            var buildFileReader = ioc.Resolve<BuildFileReader>();
-            if (!buildFileReader.TryReadBuildFile(buildFile, out root))
-            {
-                status?.Report($"ERROR: Reading Build File. {buildFile}");
-                Environment.ExitCode = 1;
-                return;
-            }
-
-            var buildFileCommands = root.Elements().Select(x => BuildCommandNode(x, null));
-
-            var buildEventValue = variables.GetValue<string>("BuildEvent", null);
-            status?.ReportHeader($"START {buildEventValue} BUILD");
-            buildFileCommands.Run(variables, commandPlugins, status);
-            status?.ReportHeader($"END {buildEventValue} BUILD");
+            status?.Report($"ERROR: Reading Build File. {buildFile}");
+            Environment.ExitCode = 1;
+            return;
         }
 
-        private static void ReportHeader(this IStatusReporter status, string message, params object[] args)
-        {
-            var formattedString = string.Format(message, args);
-            var maxLen = Math.Max(formattedString.Length + 10, 100);
-            var headPartLen = (maxLen - (formattedString.Length + 2)) / 2;
-            var headerChars = new string('#', headPartLen);
-            status.Report(string.Format($"{headerChars} {message} {headerChars}", args));
-        }
-
-        private static CommandData BuildCommandNode(XElement xElement, CommandData parent)
-        {
-            var node = new CommandData(xElement.Name.LocalName);
-            foreach (var attribute in xElement.Attributes())
-            {
-                node.Parameters[attribute.Name.LocalName] = attribute.Value;
-            }
-
-            if (!string.IsNullOrWhiteSpace(xElement.Value) && !node.Parameters.ContainsKey("value"))
-            {
-                node.Parameters["value"] = xElement.Value;
-            }
-
-            node.Parent = parent;
-            foreach (var childItem in xElement.Elements())
-            {
-                var newData = BuildCommandNode(childItem, node);
-                node.Children.Add(newData);
-            }
-
-            return node;
-        }
+        var buildEventValue = variables.GetValue<string>("BuildEvent", null);
+        status?.ReportHeader($"START {buildEventValue} BUILD");
+        buildFileCommands.Run(variables, commandPlugins, status);
+        status?.ReportHeader($"END {buildEventValue} BUILD");
     }
 
-    // BuildHelper "-bf:$(ProjectDir)$(ConfigurationName)-PostBuild.xml" '-vs_OutDir:$(OutDir)' "-vs_ConfigurationName:$(ConfigurationName)" "-vs_ProjectName:$(ProjectName)" "-vs_TargetName:$(TargetName)" "-vs_TargetPath:$(TargetPath)" "-vs_ProjectPath:$(ProjectPath)" "-vs_ProjectFileName:$(ProjectFileName)" "-vs_TargetExt:$(TargetExt)" "-vs_TargetFileName:$(TargetFileName)" "-vs_DevEnvDir:$(DevEnvDir)" "-vs_TargetDir:$(TargetDir)" "-vs_ProjectDir:$(ProjectDir)" "-vs_SolutionFileName:$(SolutionFileName)" "-vs_SolutionPath:$(SolutionPath)" "-vs_SolutionDir:$(SolutionDir)" "-vs_SolutionName:$(SolutionName)" "-vs_PlatformName:$(PlatformName)" "-vs_ProjectExt:$(ProjectExt)" "-vs_SolutionExt:$(SolutionExt)"
+    private static void ReportHeader(this IStatusReporter status, string message, params object[] args)
+    {
+        var formattedString = string.Format(message, args);
+        var maxLen = Math.Max(formattedString.Length + 10, 100);
+        var headPartLen = (maxLen - (formattedString.Length + 2)) / 2;
+        var headerChars = new string('#', headPartLen);
+        status.Report(string.Format($"{headerChars} {message} {headerChars}", args));
+    }
 }
+
+// BuildHelper "-bf:$(ProjectDir)$(ConfigurationName)-PostBuild.xml" '-vs_OutDir:$(OutDir)' "-vs_ConfigurationName:$(ConfigurationName)" "-vs_ProjectName:$(ProjectName)" "-vs_TargetName:$(TargetName)" "-vs_TargetPath:$(TargetPath)" "-vs_ProjectPath:$(ProjectPath)" "-vs_ProjectFileName:$(ProjectFileName)" "-vs_TargetExt:$(TargetExt)" "-vs_TargetFileName:$(TargetFileName)" "-vs_DevEnvDir:$(DevEnvDir)" "-vs_TargetDir:$(TargetDir)" "-vs_ProjectDir:$(ProjectDir)" "-vs_SolutionFileName:$(SolutionFileName)" "-vs_SolutionPath:$(SolutionPath)" "-vs_SolutionDir:$(SolutionDir)" "-vs_SolutionName:$(SolutionName)" "-vs_PlatformName:$(PlatformName)" "-vs_ProjectExt:$(ProjectExt)" "-vs_SolutionExt:$(SolutionExt)"
---
+++ Codefarts.BuildHelperConsoleApp/XmlBuildFileReader.cs
@@ -0,0 +1,105 @@
+﻿// <copyright file="XmlBuildFileReader.cs" company="Codefarts">
+// Copyright (c) Codefarts
+// contact@codefarts.com
+// http://www.codefarts.com
+// </copyright>
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Codefarts.BuildHelperConsoleApp;
+
+using System;
+using System.IO;
+using System.Xml.Linq;
+using Codefarts.BuildHelper;
+
+public class XmlBuildFileReader
+{
+    private IStatusReporter status;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="XmlBuildFileReader"/> class.
+    /// </summary>
+    public XmlBuildFileReader()
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="XmlBuildFileReader"/> class.
+    /// </summary>
+    /// <param name="status">A reference to a <see cref="IStatusReporter"/>.</param>
+    /// <exception cref="ArgumentNullException">If the <param name="status"/> is null.</exception>
+    public XmlBuildFileReader(IStatusReporter status)
+    {
+        this.status = status ?? throw new ArgumentNullException(nameof(status));
+    }
+
+    public bool TryReadBuildFile(string buildFile, out IEnumerable<CommandData> data)
+    {
+        // ensure file exists
+        if (buildFile == null)
+        {
+            this.status?.Report("Build file not specified.");
+            data = Enumerable.Empty<CommandData>();
+            return false;
+        }
+
+        var buildFileInfo = new FileInfo(buildFile);
+
+        // ensure file exists
+        if (!buildFileInfo.Exists)
+        {
+            this.status?.Report("Missing build file: " + buildFileInfo.FullName);
+            data = Enumerable.Empty<CommandData>();
+            return false;
+        }
+
+        this.status?.Report("Reading build file: {0}", buildFileInfo.FullName);
+
+        // read file
+        XDocument doc;
+        try
+        {
+            doc = XDocument.Parse(File.ReadAllText(buildFileInfo.FullName));
+        }
+        catch (Exception ex)
+        {
+            this.status?.Report("Error reading file: " + buildFileInfo.FullName);
+            this.status?.Report(ex.Message);
+            data = Enumerable.Empty<CommandData>();
+            return false;
+        }
+
+        if (!doc.Root.Name.LocalName.Equals("build", StringComparison.OrdinalIgnoreCase))
+        {
+            this.status?.Report("Error parsing build file: " + buildFileInfo.FullName);
+            this.status?.Report("Root node not 'build'.");
+            data = Enumerable.Empty<CommandData>();
+            return false;
+        }
+
+        this.status?.Report("... Success!");
+        data = doc.Root.Elements().Select(x => BuildCommandNode(x, null));
+        return true;
+    }
+
+    private CommandData BuildCommandNode(XElement xElement, CommandData parent)
+    {
+        var node = new CommandData(xElement.Name.LocalName);
+        foreach (var attribute in xElement.Attributes())
+        {
+            node.Parameters[attribute.Name.LocalName] = attribute.Value;
+        }
+
+        node.Parent = parent;
+        foreach (var childItem in xElement.Elements())
+        {
+            var newData = BuildCommandNode(childItem, node);
+            node.Children.Add(newData);
+        }
+
+        return node;
+    }
+}
