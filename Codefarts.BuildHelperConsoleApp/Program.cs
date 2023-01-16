03041168d0473e25b862dd13c846702cb18a4756
Dean <deanlunz@createdbyx.com>
2023-01-16 11:53:17 -0800

    Updated the main application entry point logic

--- Codefarts.BuildHelperConsoleApp/Program.cs
+++ Codefarts.BuildHelperConsoleApp/Program.cs
@@ -6,6 +6,8 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using Codefarts.BuildHelper;
 using Codefarts.DependencyInjection;
 using Codefarts.IoC;
@@ -19,25 +21,71 @@ static class Program
 {
     private static void Main(string[] args)
     {
-        var buildFile = args.FirstOrDefault(x => x.StartsWith("-b:"));
-        var solutionFile = args.FirstOrDefault(x => x.StartsWith("-s:"));
-        buildFile = string.IsNullOrWhiteSpace(buildFile) ? null : buildFile.Substring(3);
+        var buildFile = args.FirstOrDefault(x => x.StartsWith("-b:", StringComparison.InvariantCultureIgnoreCase));
+        var projectFile = args.FirstOrDefault(x => x.StartsWith("-p:", StringComparison.InvariantCultureIgnoreCase));
+        var silentMode = args.Any(x => x.Equals("-s", StringComparison.InvariantCultureIgnoreCase));
+        var targetFramework = args.FirstOrDefault(x => x.StartsWith("-tf:", StringComparison.InvariantCultureIgnoreCase));
+            
+        // validate files exist
+        if (IsFileMissing(buildFile, true, silentMode))
+        {
+            return;
+        }
+
+        if (IsFileMissing(projectFile, false, silentMode))
+        {
+            return;
+        }
+
+        // get default plugin folder
+        var appPath = Process.GetCurrentProcess().MainModule.FileName;
+        var appDir = Path.GetDirectoryName(appPath);
+        var pluginFolder = Path.Combine(appDir, "Plugins");
+
+        // store values
         var values = new Dictionary<string, object>();
         values["filename"] = buildFile;
-        values["solutionfile"] = solutionFile;
+        values["projectfile"] = projectFile;
+        values["targetframework"] = targetFramework;
+        values["applicationpath"] = appPath;
+        values["pluginfolder"] = pluginFolder;
+        var configurationManager = new ConfigManager(values);
 
+        // do initialization
         var ioc = new DependencyInjectorShim(new Container());
         ioc.Register<IDependencyInjectionProvider>(() => ioc);
-        ioc.Register<IConfigurationManager>(() => new ConfigManager(values));
+        ioc.Register<IConfigurationManager>(() => configurationManager);
         ioc.Register<IStatusReporter, ConsoleStatusReporter>();
         ioc.Register<ICommandImporter>(() => new XmlCommandFileReader(ioc));
 
+        // create app and run
         var app = ioc.Resolve<Application>();
         var result = app.Run();
         if (result.Error != null)
         {
-            Environment.ExitCode = 1;
+            Environment.ExitCode = 2;
         }
+    }
+
+    private static bool IsFileMissing(string filename, bool buildFile, bool silentMode)
+    {
+        // if file exists we are good to exit
+        var buildFileInfo = new FileInfo(filename);
+        if (buildFileInfo.Exists)
+        {
+            return false;
+        }
+
+        // file does not seem to exist
+        Environment.ExitCode = 1;
+        if (!silentMode)
+        {
+            var text = buildFile ? "Build" : "Project";
+            Console.WriteLine($"{text} file not found!");
+            Console.WriteLine($"File: {filename}");
+        }
+
+        return true;
     }
 }
 
