fce2a6cf1f6830751b5f0c7a461cd401893ecabf
Dean <deanlunz@createdbyx.com>
2023-03-12 05:43:42 -0700

    console reporter tweaks

---
+++ Codefarts.BuildHelper.ConsoleReporter/ConsoleStatusReporter.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="ConsoleStatusReporter.cs" company="Codefarts">
+// Copyright (c) Codefarts
+// contact@codefarts.com
+// http://www.codefarts.com
+// </copyright>
+
+namespace Codefarts.BuildHelper.ConsoleReporter;
+
+using System;
+using Codefarts.BuildHelper;
+
+public class ConsoleStatusReporter : IStatusReporter
+{
+    public string HeaderPrefix { get; set; }
+
+    public void Report(string message, ReportStatusType type, string category, float progress)
+    {
+        var categoryText = string.IsNullOrWhiteSpace(category) ? string.Empty : $"(Category: {category}) ";
+        var headerPrefix = string.IsNullOrWhiteSpace(this.HeaderPrefix) ? string.Empty : $"{this.HeaderPrefix}=> ";
+        var typeText = $"(Type: {type}) ";
+        var progressText = type.HasFlag(ReportStatusType.Progress) ? $"(Progress: {progress}) " : string.Empty;
+        var infoPrefix = $"{headerPrefix}{categoryText}{typeText}{progressText}";
+        infoPrefix += !string.IsNullOrEmpty(infoPrefix) ? "- " : infoPrefix;
+        Console.WriteLine(infoPrefix + $"{message}");
+    }
+}
