3eace98376a0d8b482a6128bc3803ea8f029ae51
Dean Lunz <createdbyx@createdbyx.com>
2024-06-03 23:08:32 -0700

    Removed Debug-PostBuild.xml and updated project references
    The Debug-PostBuild.xml has been removed from the Codefarts.BuildHelperConsoleApp project. The package references in the .csproj file have been updated, and several packages have been replaced with a direct reference to the project. This will ensure that the most recent version of these projects is used.

--- Codefarts.BuildHelperConsoleApp/Deploy.bat
+++
@@ -1,4 +0,0 @@
-﻿echo starting
-attrib -r "E:\BuildHelper\\*.*" /s
-rmdir /s /q  "E:\BuildHelper\"
-xcopy "P:\Code Projects\Codefarts.BuildHelper\Codefarts.BuildHelperConsoleApp\bin\Debug\net5.0\*.*" "E:\BuildHelper\" /e /y
